337c8dac47393e904f8e940d42a7e7287d1ff51f
Gerson Estuardo Alonzo Tajiboy <gersontajiboy8@gmail.com>
2025-08-11 10:07:31 -0600

    Update IPresentationMapper.java

--- src/main/java/gt/com/pharmacy/persistence/mapper/IPresentationMapper.java
+++ src/main/java/gt/com/pharmacy/persistence/mapper/IPresentationMapper.java
@@ -3,11 +3,15 @@ package gt.com.pharmacy.persistence.mapper;
 import gt.com.pharmacy.persistence.dto.PresentationDTO;
 import gt.com.pharmacy.persistence.entity.PresentationEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IPresentationMapper {
 
+    @Mapping(source = "supplier", target = "supplier")
     PresentationDTO toDto(PresentationEntity presentationEntity);
 
+    @Mapping(target = "priceHistory", ignore = true)
+    @Mapping(target = "movements", ignore = true)
     PresentationEntity toEntity(PresentationDTO presentationDTO);
 }
