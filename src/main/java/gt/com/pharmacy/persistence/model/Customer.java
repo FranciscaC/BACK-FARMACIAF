4215d48b29041c4d5e0053e650ccbda4c0d3d685
Gerson Estuardo Alonzo Tajiboy <gersontajiboy8@gmail.com>
2025-08-11 09:51:42 -0600

    Update Customer.java

--- src/main/java/gt/com/pharmacy/persistence/model/Customer.java
+++ src/main/java/gt/com/pharmacy/persistence/model/Customer.java
@@ -1,5 +1,7 @@
 package gt.com.pharmacy.persistence.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import gt.com.pharmacy.persistence.view.Views;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
@@ -17,10 +19,12 @@ public class Customer {
     @NotBlank(message = "Name cannot be blank.")
     @Size(min = 1, max = 255, message = "Name must between 1 and 255 characters.")
     @Column(name = "name")
+    @JsonView(Views.Public.class)
     private String name;
 
     @NotBlank(message = "Nit cannot be blank.")
     @Size(min = 1, max = 15, message = "Nit must between 1 and 15 characters.")
     @Column(name = "nit")
+    @JsonView(Views.Public.class)
     private String nit;
 }
