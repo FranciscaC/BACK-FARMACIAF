cba2c44e9b0d79ba4696a3155c60a1cb0f66a589
Gerson Estuardo Alonzo Tajiboy <gersontajiboy8@gmail.com>
2025-08-11 09:51:17 -0600

    Update Supplier.java

--- src/main/java/gt/com/pharmacy/persistence/model/Supplier.java
+++ src/main/java/gt/com/pharmacy/persistence/model/Supplier.java
@@ -1,5 +1,7 @@
 package gt.com.pharmacy.persistence.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import gt.com.pharmacy.persistence.view.Views;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
@@ -17,10 +19,12 @@ public class Supplier {
     @NotBlank(message = "Laboratory cannot be blank.")
     @Size(min = 1, max = 255, message = "Laboratory must between 1 and 255 characters.")
     @Column(name = "laboratory")
+    @JsonView(Views.Public.class)
     private String laboratory;
 
     @NotBlank(message = "Phone cannot be blank.")
     @Size(min = 1, max = 15, message = "Phone must between 1 and 15 characters.")
     @Column(name = "phone")
+    @JsonView(Views.Public.class)
     private String phone;
 }
